4f13188045cf6ce96f8426ce9867426a7e5b8642
Julien Knafou <julien.knafou@gmail.com>
2025-04-04 11:10:14 +0200

    refactor: reorganize data URLs and update retrieval logic for improved clarity

--- src/transcorpus/__init__.py
+++ src/transcorpus/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 home_dir = os.path.expanduser("~")
-trans_corpus_dir = os.path.join(home_dir, ".TransCorpus")
-if not os.path.exists(trans_corpus_dir):
-    os.makedirs(trans_corpus_dir)
-    print(f"Created directory: {trans_corpus_dir}")
+transcorpus_dir = os.path.join(home_dir, ".TransCorpus")
+if not os.path.exists(transcorpus_dir):
+    os.makedirs(transcorpus_dir)
+    print(f"Created directory: {transcorpus_dir}")
--- src/transcorpus/retrieval.py
+++ src/transcorpus/retrieval.py
@@ -1,10 +1,8 @@
-from transcorpus.corpus_urls import corpus_urls
+from transcorpus.data_urls import data_urls
+from transcorpus import transcorpus_dir
 import click
 import subprocess
 import os
-
-home_dir = os.path.expanduser("~")
-trans_corpus_dir = os.path.join(home_dir, ".TransCorpus")
 
 
 def download_file(url, directory):
@@ -21,39 +19,34 @@ def download_file(url, directory):
         print(f"Error downloading {url}: {e}")
 
 
+def download_data(data_type, domain_name, demo):
+    """
+    Download corpus or database from a specific domain.
+
+    """
+    if domain_name not in data_urls:
+        raise ValueError(f"Unknown domain name: {domain_name}")
+
+    if demo and data_type + "_demo" not in data_urls[domain_name]:
+        raise ValueError(f"No {data_type} demos available for: {domain_name}")
+
+    url = (
+        data_urls[domain_name]["endpoint"]
+        + data_urls[domain_name][f"{data_type}_demo" if demo else data_type]
+    )
+
+    download_file(url, transcorpus_dir + "/" + domain_name)
+
+
 @click.command()
 @click.argument("corpus_name")
 @click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
 def download_corpus(corpus_name, demo):
-    """
-    Download a corpus by name.
-
-    CORPUS_NAME: Name of the corpus to download.
-    """
-    if corpus_name not in corpus_urls:
-        raise ValueError(f"Unknown corpus name: {corpus_name}")
-
-    if demo and "files_demo" not in corpus_urls[corpus_name]:
-        raise ValueError(f"Demo files not available for corpus: {corpus_name}")
-    files = "files_demo" if demo else "files"
-
-    urls = [
-        corpus_urls[corpus_name]["endpoint"] + f
-        for f in corpus_urls[corpus_name][files]
-    ]
-
-    for url in urls:
-        download_file(url, trans_corpus_dir)
+    download_data("corpus", corpus_name, demo)
 
 
 @click.command()
 @click.argument("database_name")
 @click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
 def download_database(database_name, demo):
-    """
-    Download a database by name.
-
-    DATABASE_NAME: Name of the database to download.
-    """
-    click.echo(f"Downloading database: {database_name, demo}")
-    # Add logic for downloading the database here.
+    download_data("database", database_name, demo)
