17598f52e01b0f74015fcde13b5ba28577ee3bb3
Julien Knafou <julien.knafou@gmail.com>
2025-04-04 10:07:56 +0200

    improve cli/retrieval code structure

--- src/transcorpus/cli.py
+++ src/transcorpus/cli.py
@@ -1,4 +1,4 @@
-from transcorpus.retrieval import download_file
+from transcorpus import retrieval
 import click
 
 
@@ -7,51 +7,8 @@ def cli():
     """Transcorpus CLI tool."""
 
 
-@cli.command()
-@click.argument("database_name")
-@click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
-def download_database(database_name, demo):
-    """
-    Download a database by name.
-
-    DATABASE_NAME: Name of the database to download.
-    """
-    click.echo(f"Downloading database: {database_name, demo}")
-    # Add logic for downloading the database here.
-
-
-@cli.command()
-@click.argument("corpus_name")
-@click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
-def download_corpus(corpus_name, demo):
-    """
-    Download a corpus by name.
-
-    CORPUS_NAME: Name of the corpus to download.
-    """
-    corpus_urls = {
-        "bio": {
-            "files": ["PMID.txt", "title_abstract_en.txt"],
-            "files_demo": ["PMID_sample.txt", "title_abstract_en_sample.txt"],
-            "endpoint": "https://transcorpus.s3.text-analytics.ch/",
-        }
-    }
-
-    if corpus_name not in corpus_urls:
-        raise ValueError(f"Unknown corpus name: {corpus_name}")
-
-    if demo and "files_demo" not in corpus_urls[corpus_name]:
-        raise ValueError(f"Demo files not available for corpus: {corpus_name}")
-    files = "files_demo" if demo else "files"
-
-    urls = [
-        corpus_urls[corpus_name]["endpoint"] + f
-        for f in corpus_urls[corpus_name][files]
-    ]
-
-    for url in urls:
-        download_file(url, "data/corpus/")
-
+cli.add_command(retrieval.download_database)
+cli.add_command(retrieval.download_corpus)
 
 if __name__ == "__main__":
     cli()
--- src/transcorpus/retrieval.py
+++ src/transcorpus/retrieval.py
@@ -1,3 +1,4 @@
+import click
 import subprocess
 import os
 
@@ -14,3 +15,49 @@ def download_file(url, directory):
         subprocess.run(["wget", url, "-P", directory], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error downloading {url}: {e}")
+
+
+@click.command()
+@click.argument("corpus_name")
+@click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
+def download_corpus(corpus_name, demo):
+    """
+    Download a corpus by name.
+
+    CORPUS_NAME: Name of the corpus to download.
+    """
+    corpus_urls = {
+        "bio": {
+            "files": ["PMID.txt", "title_abstract_en.txt"],
+            "files_demo": ["PMID_sample.txt", "title_abstract_en_sample.txt"],
+            "endpoint": "https://transcorpus.s3.text-analytics.ch/",
+        }
+    }
+
+    if corpus_name not in corpus_urls:
+        raise ValueError(f"Unknown corpus name: {corpus_name}")
+
+    if demo and "files_demo" not in corpus_urls[corpus_name]:
+        raise ValueError(f"Demo files not available for corpus: {corpus_name}")
+    files = "files_demo" if demo else "files"
+
+    urls = [
+        corpus_urls[corpus_name]["endpoint"] + f
+        for f in corpus_urls[corpus_name][files]
+    ]
+
+    for url in urls:
+        download_file(url, "data/corpus/")
+
+
+@click.command()
+@click.argument("database_name")
+@click.option("--demo", "-d", is_flag=True, help="Run in demo mode.")
+def download_database(database_name, demo):
+    """
+    Download a database by name.
+
+    DATABASE_NAME: Name of the database to download.
+    """
+    click.echo(f"Downloading database: {database_name, demo}")
+    # Add logic for downloading the database here.
