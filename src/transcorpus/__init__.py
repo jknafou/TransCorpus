02df026233bbcdc3cd53df86994dfeddc4144139
Julien Knafou <julien.knafou@gmail.com>
2025-04-07 14:28:22 +0200

    feat: add create_transcorpus_dir function to handle directory creation

--- src/transcorpus/__init__.py
+++ src/transcorpus/__init__.py
@@ -2,6 +2,13 @@ import os
 
 home_dir = os.path.expanduser("~")
 transcorpus_dir = os.path.join(home_dir, ".TransCorpus")
-if not os.path.exists(transcorpus_dir):
-    os.makedirs(transcorpus_dir)
-    print(f"Created directory: {transcorpus_dir}")
+
+
+def create_transcorpus_dir():
+    if not os.path.exists(transcorpus_dir):
+        os.makedirs(transcorpus_dir)
+        print(f"Created directory: {transcorpus_dir}")
+
+
+if __name__ == "__main__":
+    create_transcorpus_dir()
